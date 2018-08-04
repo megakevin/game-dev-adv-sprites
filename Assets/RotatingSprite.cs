37d3208edb862afeccc76137cd962c29fc7ce9ee
Kevin Campusano <megakevinx@gmail.com>
2018-08-03 20:30:30 -0400

    initial commit

---
+++ Assets/RotatingSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingSprite : MonoBehaviour {
+
+    public float rotationsPerMinute = 1;
+    public bool inverseRotation = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var rotationZAngle = (this.rotationsPerMinute * 360) * Time.deltaTime;
+
+        if (this.inverseRotation)
+        {
+            rotationZAngle *= -1; 
+        }
+
+        this.transform.Rotate(0, 0, rotationZAngle);
+    }
+}
